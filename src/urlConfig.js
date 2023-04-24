6a595b6035bf3efeb0bbef0789587ab7335c839e
nhat17t3 <58282021+nhat17t3@users.noreply.github.com>
2023-04-25 00:16:39 +0700

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,8 +1,7 @@
 // const baseUrl = process.env.API || "https://"
-// const baseUrl = "http://localhost:6969";
-// const baseUrl = "https://mobilelemanh.herokuapp.com";
-const baseUrl = "http://be1.tranlan.tk";
 
+//const baseUrl = "http://be1.tranlan.tk";
+const baseUrl = "https://hoanglongnhat-nodejs-ecommerce.onrender.com";
 
 export const api = `${baseUrl}/api`;
 
